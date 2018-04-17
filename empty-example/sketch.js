40246aa711db4a0d372ee7c4e49739116877df1d
Nilesh Choudhary <nilesh.vartak@media4u.com>
2018-04-17 11:05:22 +0530

    first commit and initial setup for quad tree

---
+++ empty-example/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+
+  createCanvas(400,400);
+  // put setup code here
+}
+
+function draw() {
+  
+  background(0);
+
+  // console.log("i am superman");
+  
+  // put drawing code here
+}
